43e16a084bcff014656ce757665f097328713b29
robercano <3525807+robercano@users.noreply.github.com>
2022-12-08 18:42:25 +0100

    feat: add Game of Codes solution

--- GameOfCodes2022/solution.py
+++ GameOfCodes2022/solution.py
@@ -1,232 +1,62 @@
-import sys, os
-import copy
-
-class Node:
-    def __init__(self, letter, count, id, prev, next=None):
+class Block:
+    def __init__(self, size, letter):
+        self.size = size
         self.letter = letter
-        self.count = count
-        self.prev = prev
-        self.next = next
-        self.id = id
 
-    def __str__(self) -> str:
-        return '[' + str(self.id) + ',' + self.letter + ',' + str(self.count) + ']';
 
-    def __eq__(self, other) -> bool:
-        return other != None and self.id == other.id
+def get_basic_blocks(S):
+    allBlock = []
 
-class Graph:
-    def __init__(self, S):
-        self.head = None
-        self.tail = None
-        self.num_nodes = 0
-        self.reversed = False
-        self.id_counter = 0
+    currentLetter = ''
+    currentBlock = None
 
-        self.__build__(S)
-
-    def append(self, letter, count):
-        self.id_counter += 1
-
-        if self.head == None:
-            self.head = Node(letter, count, self.id_counter, None)
-            self.tail = self.head
+    # Collapse trivial blocks
+    for i in range(len(S)):
+        if S[i] == currentLetter:
+            currentBlock.size += 1
         else:
-            self.tail.next = Node(letter, count, self.id_counter, self.tail)
-            self.tail = self.tail.next
+            currentBlock = Block(1, S[i])
+            allBlock.append(currentBlock)
+            currentLetter = S[i]
 
-        self.num_nodes += 1
+    return allBlock
 
-    def remove(self, node):
-        if node == self.head:
-            self.head = node.next
-        if node == self.tail:
-            self.tail = node.prev
-        if node.prev != None:
-            node.prev.next = node.next
-        if node.next != None:
-            node.next.prev = node.prev
+def solution(S: str) -> int:
+    allBlock = get_basic_blocks(S)
 
-        self.num_nodes -= 1
+    letters = [chr(x) for x in range(ord('a'), ord('z') + 1)]
+    lastBlock = {k: [None, None, None, None] for k in letters}
+    lastBlockLength = {k: [0, 0, 0, 0] for k in letters}
 
-    def removeRange(self, fromNode, toNode, excludeFrom=False, excludeTo=False):
-        iter = self.iterFrom(fromNode)
-        if excludeFrom:
-            next(iter)
+    maxLengthBefore = [-1, -1, -1, 0]
 
-        for node in iter:
-            if node == toNode:
-                if excludeTo == False:
-                    self.remove(node)
-                break
+    for i in range(len(allBlock)):
+        b = allBlock[i]
+
+        for j in range(4):
+            if maxLengthBefore[j] == -1:
+                continue
+
+            currentMax = maxLengthBefore[j]
+
+            # combine from last
+            if lastBlock[b.letter][j] != None:
+                combineLength = lastBlockLength[b.letter][j] + b.size
                 
-            self.remove(node)
+                maxLengthBefore[j] = max(currentMax, combineLength)
+                
+                lastBlock[b.letter][j] = b
+                lastBlockLength[b.letter][j] = combineLength
 
-    def iterFrom(self, node):
-        return GraphIterator(self, node)
+            if j > 0: # stay
+                combineLength = currentMax + b.size
+                
+                maxLengthBefore[j - 1] = max(maxLengthBefore[j - 1], combineLength)
 
-    def reverse(self):
-        self.reversed = not self.reversed
+                lastBlock[b.letter][j - 1] = b
+                lastBlockLength[b.letter][j - 1] = max(lastBlockLength[b.letter][j - 1], combineLength)
 
-    def __build__(self, S):
-        for letter in S:
-            if self.tail == None or self.tail.letter != letter:
-                self.append(letter, 1)
-            else:
-                self.tail.count += 1
-
-    def __iter__(self):
-        return GraphIterator(self, self.head if not self.reversed else self.tail)
-
-    def __reversed__(self):
-        graph = copy.deepcopy(self)
-        graph.reversed = not graph.reversed
-        return graph
-
-    def __len__(self) -> int:
-        return self.num_nodes
-
-    def __str__(self) -> str:
-        graph_str = ''
-        for node in self:
-            graph_str += str(node)
-        return graph_str
-
-class GraphIterator:
-    def __init__(self, graph, fromNode):
-        self.current = fromNode    
-        self.reversed = graph.reversed
-        
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.current == None:
-            raise StopIteration
-        else:
-            node = self.current
-            if self.reversed:
-                self.current = self.current.prev
-            else:
-                self.current = self.current.next
-            return node
-
-def reverse(S):
-    return S[::-1]
-
-def build_graph(S):
-    graph = Graph()
-
-    for letter in S:
-        if graph.tail == None or graph.tail.letter != letter:
-            graph.append(letter, 1)
-        else:
-            graph.tail.count += 1
-
-    return graph
-
-def collapse_nodes(graph, fromNode):
-    letter_count = {}
-    max_group_count = 0
-    num_groups_to_remove = 0
-
-    print('      [COLLAPSE NODES]')
-    print('        g: ' + str(graph))
-    print('        n: ' + str(fromNode))
-
-    for node in graph.iterFrom(fromNode):
-        if node == fromNode:
-            continue
-
-        if node.letter == fromNode.letter:
-            if node.count < max_group_count or len(graph) - num_groups_to_remove < 3:
-                return False
-
-            print('        removeRange(' + str(fromNode) + ',' + str(node) + ')')
-
-            fromNode.count += node.count
-
-            graph.removeRange(fromNode, node, excludeFrom=True)
-
-            print('        graph after remove:' + str(graph))
-
-            return True
-        else:
-            if node.letter not in letter_count:
-                letter_count[node.letter] = node.count
-            else:
-                letter_count[node.letter] += node.count
-
-            if letter_count[node.letter] > max_group_count:
-                max_group_count = letter_count[node.letter]
-
-        if max_group_count > fromNode.count:
-            return False
-
-        num_groups_to_remove += 1
-    
-    return False
-
-def collapse_one_node(graph, fromNode):
-    print('  [COLLAPSE]')
-    print('    g: ' + str(graph))
-    print('    n: ' + str(fromNode))
-    print('    [REDUCE RIGHT]')
-    while collapse_nodes(graph, fromNode):
-        continue
-    
-    graph.reverse()
-
-    print('    [REDUCE LEFT]')
-    while collapse_nodes(graph, fromNode):
-        continue
-
-    graph.reverse()
-
-def find_max_node(graph, exclusion_list):
-    reduced_graph = filter(lambda node: node not in exclusion_list, graph)
-    try:
-        return max(reduced_graph, key=lambda node: node.count)
-    except ValueError:
-        return None
-
-def reduce_graph(graph):
-    exclusion_list = []
-    while True:
-        if len(graph) <= 3:
-            break
-
-        max_node = find_max_node(graph, exclusion_list)
-        if max_node == None:
-            break
-        
-        length_before = len(graph)
-        collapse_one_node(graph, max_node)
-        length_after = len(graph)
-
-        if length_before == length_after:
-            exclusion_list.append(max_node)
-        else:
-            exclusion_list = []
-
-def solution(S):
-    graph = Graph(S)
-
-    reduce_graph(graph)
-
-    sorted_groups = sorted(list(graph), key=lambda x: x.count, reverse=True)
-    first_three_groups = sorted_groups[:3]
-    first_three_counts = [group.count for group in first_three_groups]
-    
-    return sum(first_three_counts), graph
-
-# Disable
-def blockPrint():
-    sys.stdout = open(os.devnull, 'w')
-
-# Restore
-def enablePrint():
-    sys.stdout = sys.__stdout__
+    return max(maxLengthBefore[0], maxLengthBefore[1], maxLengthBefore[2], maxLengthBefore[3])
 
 TestCases = [['aabacbba', 6],
              ['aabxbaba', 6],
@@ -245,19 +75,8 @@ TestCases = [['aabacbba', 6],
              ['zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbabbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaazzzzzzzzzzzzzzzzzzzzzzzzzzzzzz', 101]]
 
 for test in TestCases:
- 
-    blockPrint()
-    print('----------------------')
-    print('String: ' + test[0])
-
-    result, graph = solution(test[0])
-
-    print('Result: ' + str(result))
-    print('Expected: ' + str(test[1]))
-    print('----------------------')
-
-    enablePrint()
-    
+    result = solution(test[0])
+  
     expected = test[1]
 
     print(test[0] + ' -> ', end='')
@@ -265,4 +84,3 @@ for test in TestCases:
         print("OK!")
     else:
         print("ERROR! (expected: " + str(expected) + ")" + " (result: " + str(result) + ")")
-        print(graph)
